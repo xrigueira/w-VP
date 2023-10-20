e24acbec0e11c2fef91b6f42bfa15a23f16d5eb2
Xurxo Rigueira Díaz <xrigueiradiaz@gmail.com>
2023-10-19 17:21:17 -0700

    test

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import matplotlib.pyplot as plt
 
 from sklearn.ensemble import RandomForestClassifier
 
+from tictoc import tictoc
+
 """This file contains the main class imRF which implements
 iterative multiresolution Random Forest."""
 
@@ -63,6 +65,10 @@ class imRF():
             # Restore the window size after recursion
             self.window_size = window_size
             return []
+    
+    def majority_vote(self, *args):
+        num_ones = sum(1 for result in args if result >= 0.51)
+        return int(num_ones > len(args) / 2)
     
     def anomalies(self):
         
@@ -266,6 +272,7 @@ class imRF():
             
         return background_indexes
     
+    @tictoc
     def init_RandomForest(self):
         
         """Initiates a set of three Random Forest classifiers
@@ -363,7 +370,8 @@ class imRF():
         pickle.dump(model_med, open(filename, 'wb'))
         filename = 'models/rf_model_low_0.sav'
         pickle.dump(model_low, open(filename, 'wb'))
-        
+    
+    @tictoc
     def RandomForest(self, iteration):
         
         """Updates the Random Forest models on each iterations.
@@ -389,9 +397,7 @@ class imRF():
         file_background.close()
         
         # Variable name change to follow best practives in ML
-        X = []
-        for i in range(len(background_windows)):
-            X.append(background_windows[i])
+        X = background_windows
 
         # # AVOID for now. Shuffle the data and variable name change to follow best practives in ML
         # X = []
@@ -418,92 +424,173 @@ class imRF():
         tree_classifications_med = [tree.predict(X[1]) for tree in trees_med]
         tree_classifications_low = [tree.predict(X[2]) for tree in trees_low]
 
-        # Get the average score for each window
+        # Get the average score for each window across all estimators
         score_Xs_high = np.mean(tree_classifications_high, axis=0)
         score_Xs_med = np.mean(tree_classifications_med, axis=0)
         score_Xs_low = np.mean(tree_classifications_low, axis=0)
-        np.save('score_Xs_high', score_Xs_high, allow_pickle=False, fix_imports=False)
-        np.save('score_Xs_med', score_Xs_med, allow_pickle=False, fix_imports=False)
-        np.save('score_Xs_low', score_Xs_low, allow_pickle=False, fix_imports=False)
         
         # plt.plot(score_Xs)
         # plt.show()
         # plt.savefig(f'images/prediction_{iteration}.png', dpi=300)
         
-        # # Get the indexes of those windows that are anomalies and background in the new data
-        # indexes_anomalies_windows = list(np.where(score_Xs >= 0.51)[0])
-        # indexes_background_windows = list(np.where(score_Xs <= 0.10)[0])
+        # Get the indexes of those windows considered anomalies or background
+        stride = 1
+        num_variables = 6
+        med_subwindow_span = len(X[1][0]) // (num_variables * stride)
+        low_subwindow_span = (len(X[0][0])- len(X[2][0])) // (num_variables * stride)
 
-        # # Extract those new anomaly and background windows
-        # add_anomalies_windows = background_windows[indexes_anomalies_windows]
+        index_high = 0
+        start_index_med, end_index_med = 0, med_subwindow_span + 1
+        start_index_low, end_index_low = 0, low_subwindow_span + 1
+
+        indexes_anomalies_windows_high, indexes_background_windows_high = [], []
+        indexes_anomalies_windows_med, indexes_background_windows_med = [], []
+        indexes_anomalies_windows_low, indexes_background_windows_low = [], []
+        for i in range(len(score_Xs_high)):
+    
+            scores_high = score_Xs_high[index_high]
+            scores_med = score_Xs_med[start_index_med:end_index_med]
+            scores_low = score_Xs_low[start_index_low:end_index_low]
+            
+            # Combine the float result with the majority voting of the lists
+            multiresolution_vote = self.majority_vote(scores_high, *scores_med, *scores_low)
+            
+            if multiresolution_vote == 1:
+                indexes_anomalies_windows_high.append(index_high)
+                indexes_anomalies_windows_med.append((start_index_med, end_index_med))
+                indexes_anomalies_windows_low.append((start_index_low, end_index_low))
+            else:
+                indexes_background_windows_high.append(index_high)
+                indexes_background_windows_med.append((start_index_med, end_index_med))
+                indexes_background_windows_low.append((start_index_low, end_index_low))
+            
+            # Update the index values
+            index_high = index_high + stride
+            start_index_med, end_index_med = start_index_med + stride, end_index_med + stride
+            start_index_low, end_index_low = start_index_low + stride, end_index_low + stride
+        
+        # Extract those new anomaly and background windows
+        add_anomalies_windows_high = [X[0][i] for i in indexes_anomalies_windows_high]
+        add_background_windows_high = [X[0][i] for i in indexes_background_windows_high]
+        add_anomalies_windows_med = []
+        [add_anomalies_windows_med.extend(X[1][start:end]) for start, end in indexes_anomalies_windows_med]
+        add_background_windows_med = []
+        [add_background_windows_med.extend(X[1][start:end]) for start, end in indexes_background_windows_med]
+        add_anomalies_windows_low = []
+        [add_anomalies_windows_low.extend(X[2][start:end]) for start, end in indexes_anomalies_windows_low]
+        add_background_windows_low = []
+        [add_background_windows_low.extend(X[2][start:end]) for start, end in indexes_background_windows_low]
         # print(f'Percentage of anomalies {round(len(add_anomalies_windows) / len(background_windows) * 100, 2)}%')
-        # add_background_windows = background_windows[indexes_background_windows]
 
-        # # Read the previous windowed anomalous data
-        # file_anomalies = open(f'pickels/anomaly_data_{iteration - 1}.pkl', 'rb')
-        # prev_anomalies_windows = pickle.load(file_anomalies)
-        # file_anomalies.close()
+        # Read the previous windowed anomalous data
+        file_anomalies = open(f'pickels/anomaly_data_{iteration - 1}.pkl', 'rb')
+        prev_anomalies_windows = pickle.load(file_anomalies)
+        file_anomalies.close()
 
-        # # Read the previous windows background
-        # file_background = open(f'pickels/background_data_{iteration - 1}.pkl', 'rb')
-        # prev_background_windows = pickle.load(file_background)
-        # file_background.close()
+        # Read the previous windows background
+        file_background = open(f'pickels/background_data_{iteration - 1}.pkl', 'rb')
+        prev_background_windows = pickle.load(file_background)
+        file_background.close()
         
-        # # Conactenate new data with old data
-        # anomalies_windows = np.vstack((prev_anomalies_windows, add_anomalies_windows))
-        # background_windows = np.vstack((prev_background_windows, add_background_windows))
-
-        # # Save anomalies_data to disk as pickle object
-        # with open(f'pickels/anomaly_data_{iteration}.pkl', 'wb') as file:
-        #     pickle.dump(anomalies_windows, file)
+        # Conactenate new data with old data
+        anomalies_windows = [np.vstack((prev_anomalies_windows[0], add_anomalies_windows_high)),
+                            np.vstack((prev_anomalies_windows[1], add_anomalies_windows_med)),
+                            np.vstack((prev_anomalies_windows[2], add_anomalies_windows_low))]
         
-        # # Save background data as a pickle object
-        # with open(f'pickels/background_data_{iteration}.pkl', 'wb') as file:
-        #     pickle.dump(background_windows, file)
+        background_windows = [np.vstack((prev_background_windows[0], add_background_windows_high)),
+                            np.vstack((prev_background_windows[1], add_background_windows_med)),
+                            np.vstack((prev_background_windows[2], add_background_windows_low))]
+
+        # Save anomalies_data to disk as pickle object
+        with open(f'pickels/anomaly_data_{iteration}.pkl', 'wb') as file:
+            pickle.dump(anomalies_windows, file)
         
-        # # Retrain the model with the updated anomaly and background data
-        # # Generate labels for each window
-        # anomalies_labels = np.array([1 for i in anomalies_windows])
-        # background_labels = np.array([0 for i in background_windows])
+        # Save background data as a pickle object
+        with open(f'pickels/background_data_{iteration}.pkl', 'wb') as file:
+            pickle.dump(background_windows, file)
         
-        # # Concatenate arrays
-        # X = np.concatenate((anomalies_windows, background_windows))
-        # y = np.concatenate((anomalies_labels, background_labels))
-
-        # # Shuffle data
-        # combined = np.column_stack((X, y))
-        # np.random.seed(self.seed)
-        # np.random.shuffle(combined)
-
-        # # Split the shuffled array back into data and labels
-        # X, y = combined[:, :-1], combined[:, -1]
-
-        # # Load the model
-        # filename = f'models/rf_model_{iteration - 1}.sav'
-        # model = pickle.load(open(filename, 'rb'))
-
-        # # Increase estimators and set warm_start to True
-        # model.n_estimators += 10
-        # model.warm_start = True
-
-        # # Split the shuffled data into the training and testing set
-        # X_train, y_train = X[:int(len(X) * 0.75)], y[:int(len(X) * 0.75)]
-        # X_test, y_test = X[int(len(X) * 0.75):], y[int(len(X) * 0.75):]
-
-        # # Fit the model to the training data
-        # model.fit(X_train, y_train)
+        # Retrain the model with the updated anomaly and background data
+        anomalies_labels = []
+        for i in range(len(anomalies_windows)):
+            anomalies_labels.append(np.array([1 for j in anomalies_windows[i]]))
         
-        # from sklearn.metrics import confusion_matrix as cm
-        # confusion_matrix = cm(y_test, model.predict(X_test))
-        # print(confusion_matrix)
+        background_labels = []
+        for i in range(len(background_windows)):
+            background_labels.append(np.array([0 for j in background_windows[i]]))
+        
+        # Concatenate array
+        X = []
+        for i in range(len(anomalies_windows)):
+            X.append(np.concatenate((anomalies_windows[i], background_windows[i])))
+        
+        y = []
+        for i in range(len(anomalies_windows)):
+            y.append(np.concatenate((anomalies_labels[i], background_labels[i])))
 
-        # # Get the number of rows labeled as anomalies in y_test
-        # num_anomalies = len([i for i in y_test if i==1])
-        # print('Number of anomalies in test set:', num_anomalies)
+        # Shuffle data
+        randomized = []
+        for i in range(len(anomalies_windows)):
+            combined = np.column_stack((X[i], y[i]))
+            np.random.seed(self.seed)
+            np.random.shuffle(combined)
+            randomized.append(combined)
+            
+        # Split the shuffled array back into data and labels
+        for i in range(len(anomalies_windows)):    
+            X[i], y[i] = randomized[i][:, :-1], randomized[i][:, -1]
 
-        # # Save the model to disk
-        # filename = f'models/rf_model_{iteration}.sav'
-        # pickle.dump(model, open(filename, 'wb'))
+        # Load the models
+        filename = f'models/rf_model_high_{iteration - 1}.sav'
+        model_high = pickle.load(open(filename, 'rb'))
+        filename = f'models/rf_model_med_{iteration - 1}.sav'
+        model_med = pickle.load(open(filename, 'rb'))
+        filename = f'models/rf_model_low_{iteration - 1}.sav'
+        model_low = pickle.load(open(filename, 'rb'))
+
+        # Increase estimators and set warm_start to True
+        model_high.n_estimators += 10
+        model_high.warm_start = True
+        model_med.n_estimators += 10
+        model_med.warm_start = True
+        model_low.n_estimators += 10
+        model_low.warm_start = True
+
+        # Split the shuffled data into the training and testing set
+        X_train, y_train, X_test, y_test = [], [], [], []
+        for i in range(len(anomalies_windows)):
+            X_train.append(X[i][:int(len(X[i]) * 0.75)])
+            y_train.append(y[i][:int(len(X[i]) * 0.75)])
+            X_test.append(X[i][int(len(X[i]) * 0.75):])
+            y_test.append(y[i][int(len(X[i]) * 0.75):])
+
+        # Fit the model to the training data
+        model_high.fit(X_train[0], y_train[0]) # Long length data windows
+        model_med.fit(X_train[1], y_train[1]) # Medium legth data windows
+        model_low.fit(X_train[2], y_train[2]) # Short length data windows
+
+        from sklearn.metrics import confusion_matrix as cm
+        confusion_matrix_high = cm(y_test[0], model_high.predict(X_test[0]))
+        print(confusion_matrix_high)
+        confusion_matrix_med = cm(y_test[1], model_med.predict(X_test[1]))
+        print(confusion_matrix_med)
+        confusion_matrix_low = cm(y_test[2], model_low.predict(X_test[2]))
+        print(confusion_matrix_low)
+        
+        # Get the number of rows labeled as anomalies in y_test
+        num_anomalies = len([i for i in y_test[0] if i==1])
+        print('Number of anomalies in test set:', num_anomalies)
+        num_anomalies = len([i for i in y_test[1] if i==1])
+        print('Number of anomalies in test set:', num_anomalies)
+        num_anomalies = len([i for i in y_test[2] if i==1])
+        print('Number of anomalies in test set:', num_anomalies)
+        
+        # Save the model to disk
+        filename = f'models/rf_model_high_{iteration}.sav'
+        pickle.dump(model_high, open(filename, 'wb'))
+        filename = f'models/rf_model_med_{iteration}.sav'
+        pickle.dump(model_med, open(filename, 'wb'))
+        filename = f'models/rf_model_low_{iteration}.sav'
+        pickle.dump(model_low, open(filename, 'wb'))
 
 if __name__ == '__main__':
     
@@ -513,7 +600,7 @@ if __name__ == '__main__':
                 window_size=window_size, stride=1, seed=0)
     
     # Implement iterative process
-    for i in range(0, 2):
+    for i in range(0, 5):
         
         if i == 0:
             print(f'[INFO] Iteration {i}')
--- obs.py
+++ obs.py
@@ -33,3 +33,88 @@
 #     windows = np.array(windows)
     
 #     return windows
+
+# Multiresolution windower test code
+# import numpy as np
+
+# data = [[*range(1, 65, 1)]]
+
+# def windower(data, window_size, stride, num_variables):
+
+#     windows = []
+#     if window_size > 1:
+#         for i in data:
+#             num_windows = (len(i) - window_size * num_variables) // (stride * num_variables) + 1
+#             for j in range(0, num_windows, stride):
+#                 window = i[j * num_variables: (j * num_variables) + (window_size * num_variables)]
+#                 windows.append(window)
+#         window_size = window_size // 2
+#         return [windows] + windower(data, window_size, stride, num_variables)
+#     else:
+#         return []
+
+# window_size = 8
+# num_variables = 6
+# stride = 1
+
+# windows = windower(data, window_size, stride, num_variables)
+
+# Multiresolution combinator test code
+# import pickle
+# import numpy as np
+
+# # Perform majority voting
+# def majority_vote(*args):
+#     num_ones = sum(1 for result in args if result >= 0.51)
+#     return int(num_ones > len(args) / 2)
+
+# # Read the windowed anomalous data
+# file_anomalies = open('pickels/anomaly_data_0.pkl', 'rb')
+# anomalies_windows = pickle.load(file_anomalies)
+# file_anomalies.close()
+
+# file_anomalies = open('pickels/background_data_0.pkl', 'rb')
+# background_windows = pickle.load(file_anomalies)
+# file_anomalies.close()
+
+# score_Xs_high = np.load('score_Xs_high.npy', allow_pickle=False, fix_imports=False)
+# score_Xs_med = np.load('score_Xs_med.npy', allow_pickle=False, fix_imports=False)
+# score_Xs_low = np.load('score_Xs_low.npy', allow_pickle=False, fix_imports=False)
+
+# # Now lets try to get the scores for the first high window across all levels
+# stride = 1
+# num_variables = 6
+# med_subwindow_span = len(background_windows[1][0]) // (num_variables * stride)
+# low_subwindow_span = (len(background_windows[0][0])- len(background_windows[2][0])) // (num_variables * stride)
+
+# index_high = 0
+# start_index_med, end_index_med = 0, med_subwindow_span + 1
+# start_index_low, end_index_low = 0, low_subwindow_span + 1
+
+# indexes_anomalies_windows_high, indexes_background_windows_high = [], []
+# indexes_anomalies_windows_med, indexes_background_windows_med = [], []
+# indexes_anomalies_windows_low, indexes_background_windows_low = [], []
+# for i in range(len(score_Xs_high)):
+    
+#     scores_high = score_Xs_high[index_high]
+#     scores_med = score_Xs_med[start_index_med:end_index_med]
+#     scores_low = score_Xs_low[start_index_low:end_index_low]
+    
+#     # Combine the float result with the majority voting of the lists
+#     multiresolution_vote = majority_vote(scores_high, *scores_med, *scores_low)
+    
+#     if multiresolution_vote == 1:
+#         indexes_anomalies_windows_high.append(index_high)
+#         indexes_anomalies_windows_med.append((start_index_med, end_index_med))
+#         indexes_anomalies_windows_low.append((start_index_low, end_index_low))
+#     else:
+#         indexes_background_windows_high.append(index_high)
+#         indexes_background_windows_med.append((start_index_med, end_index_med))
+#         indexes_background_windows_low.append((start_index_low, end_index_low))
+    
+#     # Update the index values
+#     index_high = index_high + stride
+#     start_index_med, end_index_med = start_index_med + stride, end_index_med + stride
+#     start_index_low, end_index_low = start_index_low + stride, end_index_low + stride
+    
+# print(indexes_anomalies_windows_low)
